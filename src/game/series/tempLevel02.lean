c7f4826cfef1698e8d6d75b4da258ed9c55f58a2
Dan Stanescu <stanescu@uwyo.edu>
2020-04-16 14:16:15 -0600

    Series temp ideas

---
+++ src/game/series/tempLevel02.lean
@@ -0,0 +1,18 @@
+variable X : Type -- hide
+
+/- 
+Idea 02: if $\forall n \in \mathbb{N}, a_n \ge 0$,
+$\sum a_n$ converges iff partial sums bounded above.
+-/
+
+
+/- Lemma
+If $A$ and $B$ are sets of any type $X$, then
+$$ A \subseteq A\cup B.$$ 
+-/
+theorem subset_union_left (A B : set X) : A ⊆ A ∪ B :=
+begin
+    --change ∀ (x : α), x ∈ A → x ∈ A ∪ B,  --they may want to do this
+    intros x hx,
+    left, exact hx, done
+end
---
+++ src/game/series/tempLevel03.lean
@@ -0,0 +1,17 @@
+variable X : Type -- hide
+
+/- 
+Idea 03: comparison test
+-/
+
+
+/- Lemma
+If $A$ and $B$ are sets of any type $X$, then
+$$ A \subseteq A\cup B.$$ 
+-/
+theorem subset_union_left (A B : set X) : A ⊆ A ∪ B :=
+begin
+    --change ∀ (x : α), x ∈ A → x ∈ A ∪ B,  --they may want to do this
+    intros x hx,
+    left, exact hx, done
+end
