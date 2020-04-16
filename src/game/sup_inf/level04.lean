64f33d3ec5906b22febcbd84e9f719e552341db2
Dan Stanescu <stanescu@uwyo.edu>
2020-04-16 13:28:49 -0600

    Extra sup_inf levels

---
+++ src/game/sup_inf/level04.lean
@@ -0,0 +1,50 @@
+import data.real.basic
+
+definition is_upper_bound (S : set ℝ) (x : ℝ) := ∀ s ∈ S, s ≤ x 
+definition is_lub' (S : set ℝ) (x : ℝ) := is_upper_bound S x ∧ 
+  ∀ y : ℝ, is_upper_bound S y → x ≤ y
+definition has_lub (S : set ℝ) := ∃ x, is_lub' S x 
+
+lemma two_real_ne_zero : (2:ℝ) ≠ 0 :=
+begin
+    intro, linarith,
+end
+
+lemma avg_lt_max {mn mx: ℝ} (H : mn < mx) : (mn+mx) / 2 < mx :=
+begin
+  apply (mul_lt_mul_right (show (0:ℝ)<2, by norm_num)).1,
+  rw [div_mul_cancel _ (two_real_ne_zero)],
+  simp [H,mul_two],
+end
+
+lemma min_lt_avg {mn mx: ℝ} (H : mn < mx) : mn < (mn+mx) / 2 :=
+begin
+  apply (mul_lt_mul_right (show (0:ℝ)<2, by norm_num)).1,
+  rw [div_mul_cancel _ (two_real_ne_zero)],
+  simp [H,mul_two],
+end
+
+lemma lub_open (y : ℝ) : is_lub' {x : ℝ | x < y} y :=
+begin
+split,
+{ intros a ha,
+  exact le_of_lt ha, 
+},
+--unfold lower_bounds,
+intro b,
+intro Hb,
+refine le_of_not_gt _,
+intro Hnb,
+let c:=(b+y)/2,
+--unfold upper_bounds at Hb,
+have H2 := Hb c,
+clear Hb,
+have H : c ∈ {x : ℝ | x < y},
+{ exact avg_lt_max Hnb,
+},
+have Hcleb := H2 H,
+have Hbltc : b < c := min_lt_avg Hnb,
+exact not_lt.2 Hcleb Hbltc,
+end
+
+
---
+++ src/game/sup_inf/lub_rational.lean
@@ -0,0 +1,20 @@
+import data.real.basic
+
+definition is_upper_bound (S : set ℝ) (x : ℝ) := ∀ s ∈ S, s ≤ x 
+definition is_lub' (S : set ℝ) (x : ℝ) := is_upper_bound S x ∧ 
+  ∀ y : ℝ, is_upper_bound S y → x ≤ y
+definition has_lub (S : set ℝ) := ∃ x, is_lub' S x 
+
+def S3b : set ℝ := {x : ℝ | ∃ y : ℚ, x = ↑y}
+
+theorem Q3b : ∀ b : ℝ, ¬ (is_lub (S3b) b) :=
+begin
+intros b Hlub,
+have Hbub : b ∈ upper_bounds S3b := Hlub.left,
+have H : b < (b+1) := calc b = b+0 : (add_zero _).symm
+                         ... < b+1 : add_lt_add_left zero_lt_one _,
+cases (exists_rat_btwn H) with q Hq,
+have Hqin : ↑q ∈ S3b := ⟨q,rfl⟩,
+have Hwrong2 := Hbub Hqin,
+exact not_lt.2 Hwrong2 (Hq.left),
+end
