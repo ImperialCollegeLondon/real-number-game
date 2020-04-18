8828715d294ce3503d82a41e5f8e405b2b42f278
Dan Stanescu <stanescu@uwyo.edu>
2020-04-18 17:34:41 -0600

    Correction series 01

--- src/game/series/tempLevel01.lean
+++ src/game/series/tempLevel01.lean
@@ -1,14 +1,14 @@
 import game.series.L01defs
 
 /- 
-Temporary level 01: $\sum a_n$ converges iff $a_n \to 0$.
+Temporary level 01: If $\sum a_n$ converges, then $a_n \to 0$.
 -/
 
 /- Lemma
-Partial sum sequence of $a_n$ convergent iff $a_n → 0$.
+If partial sum sequence of $a_n$ convergent, $a_n → 0$.
 -/
 lemma sum_converges (a : ℕ → ℝ) : 
-    is_convergent (partial_sum_sequence a) ↔ is_limit a 0 :=
+    is_convergent (partial_sum_sequence a) \to is_limit a 0 :=
 begin
     sorry,
 end
