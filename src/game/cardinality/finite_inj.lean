4f0213dff475eef04914ebacb33db072e2af87d6
stanescuUW <stanescu@uwyo.edu>
2020-04-26 14:55:25 -0600

    Cardi start

--- src/game/cardinality/finite_inj.lean
+++ src/game/cardinality/finite_inj.lean
@@ -16,8 +16,8 @@ local attribute [instance] classical.prop_decidable
 -- end hide
 
 /- Lemma
-If $f : X \to Y$ is an injective function and $Y$ is countable, then
-$X$ is also countable.
+If $f : X \to Y$ is an injective function and $Y$ is finite, then
+$X$ is also finite.
 -/
 theorem finite_inj (X Y : set ℝ) (f : X → Y) (hY : finite Y) : 
     injective f → finite X :=
