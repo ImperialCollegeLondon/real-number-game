4e6db85eb648a216da6bb905b18d5ebbfa43f94e
stanescuUW <stanescu@uwyo.edu>
2020-05-18 14:01:34 -0600

    Proof order/level05

--- src/game/order/level05.lean
+++ src/game/order/level05.lean
@@ -18,7 +18,32 @@ $$| |a| - |b| | ≤ |a - b|$$.
 -/
 theorem abs_of_sub_le_abs (a b : ℝ) : | |a| - |b| | ≤ |a - b| :=
 begin
-    sorry, -- need to work it out
+    have h1 : a = (a - b) + b, norm_num,
+    have h2 : |a| = |(a-b) + b|, rw h1, simp,
+    have h3 : |(a-b) + b | ≤ |a-b| + |b|, exact abs_add _ _,
+    rw ← h2 at h3,
+    have h4a : |a| - |b| ≤ |a - b|, linarith,
+    clear h1 h2 h3,
+    have h1 : b = (b - a) + a, norm_num,
+    have h2 : |b| = |(b-a) + a|, rw h1, simp,
+    have h3 : |(b-a) + a | ≤ |b-a| + |a|, exact abs_add _ _,
+    rw ← h2 at h3,
+    have h4b : |b| - |a| ≤ |b - a|, linarith, 
+    clear h1 h2 h3,
+    have h1 := eq.symm ( abs_neg (a-b) ),
+    have h2 : -(a-b) = b - a, norm_num,
+    rw h2 at h1, clear h2,
+    rw ← h1 at h4b, clear h1,
+    have H : max ( |a| - |b| ) ( |b| - |a| ) ≤ | a - b |, 
+        simp, split, exact h4a, exact h4b,
+    unfold abs,
+    unfold abs at H, 
+    have G: -(max a (-a) - max b (-b)) = max b (-b) - max a (-a),
+        norm_num,
+    rw G,
+    exact H, done,
 end
 
 end xena --hide
+
+example (a: ℝ) : |a| = | -a| := by library_search
