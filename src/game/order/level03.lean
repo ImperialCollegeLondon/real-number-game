eb873bc132594e759b1117758bd3734688c62bb1
stanescuUW <stanescu@uwyo.edu>
2020-05-18 13:20:49 -0600

    Proof in order/level03

--- src/game/order/level03.lean
+++ src/game/order/level03.lean
@@ -16,9 +16,24 @@ notation `|` x `|` := abs x --hide
 For any two real numbers $a$ and $b$, we have that
 $$|a| ≤ c ↔ -c ≤ a ≤ c$$.
 -/
-theorem abs_le_rng (a c : ℝ) (h : 0 ≤ c): |a| ≤ c → (-c) ≤ a ∧ a ≤ c :=
+theorem abs_le (a c : ℝ) (h : 0 ≤ c): |a| ≤ c → (-c) ≤ a ∧ a ≤ c :=
 begin
-    sorry,
+    rcases lt_trichotomy a 0 with haNeg | haZero | haPos,
+    { -- case a < 0
+        intro H, 
+        have h1 : | a | = - a, exact abs_of_neg haNeg,
+        rw h1 at H, split, linarith, linarith,
+    },
+    { -- case a = 0
+        intro H, rw haZero, split, linarith, exact h,
+    },
+    { -- case 0 < a
+        intro H,
+        have h1 : |a| = a, exact abs_of_pos haPos,
+        rw h1 at H, split, linarith, exact H,
+    },
+    done
 end
 
 end xena --hide
+
