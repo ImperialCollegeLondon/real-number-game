a270e4134cdaa36459597cd569987edd0e92aac8
stanescuUW <stanescu@uwyo.edu>
2020-05-20 11:56:39 -0600

    Proof in /topology

--- src/game/topology/union_open_sets.lean
+++ src/game/topology/union_open_sets.lean
@@ -7,9 +7,11 @@ open set
 namespace xena
 -- This will eventually be the first level, containing basic definitions
 -- Work in progress
+-- First I used ⊂ in the definition, then changed to ⊆ 
+-- in order to be able to use subset.trans -- DS
 -- end hide
 
-def is_open (X : set ℝ) := ∀ x ∈ X, ∃ δ > 0, { y : ℝ  | x - δ < y ∧ y < x + δ } ⊂ X
+def is_open (X : set ℝ) := ∀ x ∈ X, ∃ δ > 0, { y : ℝ  | x - δ < y ∧ y < x + δ } ⊆ X
 variable β : Type
 
 -- begin hide
@@ -23,7 +25,18 @@ Arbitrary union of open sets is open -- WIP, to do.
 lemma is_open_union_of_open (X : β → set ℝ ) ( hj : ∀ j, is_open (X j) )
     : is_open (Union X) :=
 begin
-    sorry,
+    intros x hx,
+    simp at hx,
+    cases hx with i hi,
+    have H := hj i,
+    have G := H x hi,
+    cases G with d hd, use d,
+    cases hd with hd1 hd2,
+    split, exact hd1,
+    have hd3 : X i ⊆ Union X, 
+        intros t ht, simp,
+        use i, exact ht,
+    exact subset.trans hd2 hd3, done
 end
 
 
