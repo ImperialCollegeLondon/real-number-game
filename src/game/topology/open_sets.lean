ac05bea9b3ec0c3a2e042d587c2f3a5facd2facc
stanescuUW <stanescu@uwyo.edu>
2020-05-19 18:50:20 -0600

    Cleaner statement

--- src/game/topology/open_sets.lean
+++ src/game/topology/open_sets.lean
@@ -21,17 +21,23 @@ variable J : set (set ℝ)
 variable f : I → set ℝ 
 #check Union f
 variable β : Type
+variable K : set β
+#check K
 lemma arbitrary_union_open_sets_v1 (J : set β ) (f : J → set ℝ ) ( hj : ∀ j : J, is_open (f j) )
+-- I'd rather write `hj : ∀ j ∈ J`, but not sure how to handle that `has_mem` yet
+-- Is that possible? Is it desirable?
     : is_open (Union f) := sorry
+lemma arbitrary_union_open_sets_v2 :
+    ∀ J : set β,
+    ∀ f : J → set ℝ,
+    ∀ j, is_open (f j)  → is_open (Union f) := sorry
 -- end hide
 
 /- Lemma
 Arbitrary union of open sets is open -- WIP, to do.
 Could also make J simpler-looking, as above.
 -/
-lemma arbitrary_union_open_sets (J : set (set ℝ) ) (f : J → set ℝ ) ( hj : ∀ j : J, is_open (f j) )
--- I'd rather write ∀ j ∈ J, but not sure how to handle that `has_mem` yet
--- Is that possible? Is it desirable?
+lemma arbitrary_union_open_sets (J : set β) (f : J → set ℝ ) ( hj : ∀ j, is_open (f j) )
     : is_open (Union f) :=
 begin
     sorry,
