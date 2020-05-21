300a53b96190054b1a2803d60c765d4865b4c572
Gavin <gavin.r.thomson@gmail.com>
2020-05-21 15:17:29 +0100

    moved set levels around

--- src/game/sets/sets_level07.lean
+++ src/game/sets/sets_level07.lean
@@ -1,4 +1,5 @@
-import data.real.basic --hide
+import tactic --hide
+variable X : Type --hide
 
 /-
 # Chapter 1 : Sets
@@ -6,27 +7,53 @@ import data.real.basic --hide
 ## Level 7
 -/
 
-
-/- 
-This is a very basic example of working with intervals of real numbers in Lean.
-An interval that is closed at both endpoints $a$ and $b$ can be 
-constructed using `set.Icc a b`. For an open-closed interval, the notation
-is `set.Ioc a b`, etc. The usual closed-interval notation, using square
-brackets, is used here as a wrapper around these definitions.
-After `intro hx,` the `split` tactic will showcase the conditions for 
-membership. The inequality goals can be met with the `linarith` tactic.
-The latter is very useful when dealing with goals that don't involve any
-nonlinearity in the involved variables, in particular with inequalities.
+/-
+In this level you continue working with sets, while learning some useful
+Lean tactics, `by_contradiction` and `push_neg`. 
+You need to prove that the empty set is included in any set.
+To do that, we'll follow the advice of P. Halmos in "Naive Set Theory".
+That is, to prove something is true about the empty set, prove that it cannot be false.
 -/
 
-notation `[` a `,` b `]`  := set.Icc a b
+/- Hint : Empty set definition in mathlib: 
+theorem empty_def : (∅ : set α) = {x | false} := rfl
+-/
+-- begin hide
+-- Is there a definition in Lean core that I'm missing?
+-- end hide
 
+/-
+When starting this level, the goal is:
+$$\forall (A : \textrm{set} \; X), \; \emptyset \subseteq A$$
+To make progress, you'll need to instantiate a specific set $A$ 
+with the well-known `intro` tactic.
+
+Remember now the definition of set inclusion. The goal 
+$$ \emptyset \subseteq A$$
+is the same as 
+$$ \forall x \in \emptyset, x \in A.$$
+Thus, use `change ∀ x ∈ ∅, x ∈ A` on the line after `intro A`.
+Time to switch to the meat of the proof. Type `by_contradiction hn` to introduce 
+the contradiction hypothesis `hn` and change the goal to `false`.
+ 
+Next, it is helpful to further change this into the equivalent statement that there exists 
+a member of the empty set which does not belong to $A$. 
+This can be accomplished by
+`push_neg at hn`. You sure can bring this home from here using `cases`.
+
+-/
+
+local attribute [instance] classical.prop_decidable --hide
 /- Lemma
-If $x = 2$ then $x ∈ [0,5]$
+The empty set is included in any set $A$.... 
 -/
-lemma in_closed_interval (x:ℝ) : x = 2 → x ∈ [(0:ℝ), 5] := 
+theorem empty_set_subset : ∀ A : set X, ∅ ⊆ A :=
 begin
-    intro hx,
-    split, linarith, linarith, done
+    intro A, 
+    change ∀ x ∈ ∅, x ∈ A,
+    by_contradiction hn,
+    push_neg at hn,
+    cases hn with x h1,
+    cases h1 with hf ha,
+    exact hf, done
 end
-
--- src/game/sets/sets_level08.lean
+++ src/game/sets/sets_level08.lean
@@ -1,4 +1,4 @@
-import data.real.basic
+import data.real.basic --hide
 
 /-
 # Chapter 1 : Sets
@@ -6,27 +6,27 @@ import data.real.basic
 ## Level 8
 -/
 
+
 /- 
-This is a little more complicated example asking you to work with intervals of reals.
-The result will be of help in the sup-inf world.
+This is a very basic example of working with intervals of real numbers in Lean.
+An interval that is closed at both endpoints $a$ and $b$ can be 
+constructed using `set.Icc a b`. For an open-closed interval, the notation
+is `set.Ioc a b`, etc. The usual closed-interval notation, using square
+brackets, is used here as a wrapper around these definitions.
+After `intro hx,` the `split` tactic will showcase the conditions for 
+membership. The inequality goals can be met with the `linarith` tactic.
+The latter is very useful when dealing with goals that don't involve any
+nonlinearity in the involved variables, in particular with inequalities.
 -/
 
 notation `[` a `,` b `]`  := set.Icc a b
 
-def mem_prod_sets (A : set ℝ) (B : set ℝ) := { x : ℝ | ∃ y ∈ A, ∃ z ∈ B, x = y * z}
-
-
 /- Lemma
-Prove that if $x = 0$, then `x ∈ mem_prod_sets [(-2:ℝ),-1] [(0:ℝ), 3]`
+If $x = 2$ then $x ∈ [0,5]$
 -/
-lemma zero_in_prod : (0:ℝ) ∈ mem_prod_sets [(-2:ℝ), -1] [(0:ℝ), 3] :=
+lemma in_closed_interval (x:ℝ) : x = 2 → x ∈ [(0:ℝ), 5] := 
 begin
-  set a0 := (0:ℝ) with ha,
-  have h1 : a0 ∈ (set.Icc (0:ℝ) 3), simp, linarith,
-  set b := (-2:ℝ) with hb,
-  have h2 : b ∈ set.Icc (-2:ℝ) (-1:ℝ), simp, linarith,
-  use b, split, exact h2,
-  use a0, split, exact h1, rw ha, norm_num, done
+    intro hx,
+    split, linarith, linarith, done
 end
-
 
