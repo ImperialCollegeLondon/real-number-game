9200c61ef358ce53831f6264333c3f3bf83b2248
Dan Stanescu <stanescu@uwyo.edu>
2020-06-02 13:21:25 -0600

    Equivalent continuity defs

--- src/game/functions/seqContinDef.lean
+++ src/game/functions/seqContinDef.lean
@@ -1,6 +1,11 @@
 import data.real.basic
+
+namespace xena
+
 open function
 open real
+
+open_locale classical
 
 /-
 Classic eps-delta definition of continuity is equivalent to 
@@ -14,7 +19,7 @@ def is_limit (a : ℕ → ℝ) (l : ℝ) :=
     ∀ ε : ℝ, 0 < ε → ∃ N : ℕ, ∀ n : ℕ, N ≤ n → |a n - l| < ε
 
 def continuous_at_x (f : ℝ → ℝ) (x : ℝ) := 
-    ∀ ε : ℝ, 0 < ε → ∃ δ : ℝ, 0 < δ ∧ ∀ y : ℝ, |x - y| < δ → |f x - f y| < ε
+    ∀ ε : ℝ, 0 < ε → ∃ δ : ℝ, 0 < δ ∧ ∀ y : ℝ, |y - x| < δ → |f y - f x| < ε
 def continuous_at_x_seq (f : ℝ → ℝ) (x : ℝ) :=
     ∀ (a : ℕ → ℝ), is_limit a x → is_limit ( λ n : ℕ, f (a n) ) (f x)
 
@@ -24,5 +29,42 @@ The two definitions of continuity are equivalent.
 lemma continuous_iff (f : ℝ → ℝ) : 
     ∀ x : ℝ, continuous_at_x f x ↔ continuous_at_x_seq f x :=
 begin
-    sorry,
+    intro x,
+    split,
+    { -- classical continuity def -> sequence def
+        intros H a hax e he,
+        have h1 := H e he,
+        cases h1 with d hdd,
+        cases hdd with hd hy,
+        have h2 := hax d hd,
+        cases h2 with N hN,
+        use N,
+        intros n hn,
+        have hnd := hN n hn,
+        have G := hy (a n) hnd, exact G,
+    },
+    { -- sequence def -> classical def is a little trickier
+      -- proof by contradiction
+      contrapose!,
+      intro H, unfold continuous_at_x at H,
+      push_neg at H,
+      cases H with e hee,
+      cases hee with he hdd,
+      unfold continuous_at_x_seq, push_neg,
+      let a : ℕ → ℝ := λ n, x + 1 / (2*(n+1)),
+      use a,
+      split,
+      -- prove this sequence does converge to x
+      intros ε hε,
+      use ceil ( (1:ℝ)/((2:ℝ)*ε)  ), -- this fails
+      
+      -- but f(a n) does not converge to f(x)
+
+
+    },
+    done
 end
+
+end xena
+
+#check ceil
