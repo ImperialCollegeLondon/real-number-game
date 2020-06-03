5f011c30dcd97ab9f9fd2154f9f28c8c738b2720
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-06-03 13:48:30 +0100

    not sure if we care about this in mathlib

--- src/for_mathlib/stuff.lean
+++ src/for_mathlib/stuff.lean
@@ -1,19 +1,22 @@
-import algebra.pointwise
+--import algebra.pointwise tactic
 
--- /-- Makes an add_comm_monoid in Lean's sense from a more minimal
--- set of axioms, deducing zero_add from add_comm and add_zero
--- -/
--- def add_comm_monoid.mk' {M : Type*} [has_add M] [has_zero M] 
--- (add_assoc : ∀ a b c : M, a + b + c = a + (b + c))
--- (add_zero : ∀ a : M, a + 0 = a)
--- (add_comm : ∀ a b : M, a + b = b + a) :
--- add_comm_monoid M :=
--- { add := (+),
---   add_assoc := add_assoc,
---   zero := (0),
---   add_zero := add_zero,
---   add_comm := add_comm,
---   zero_add := λ a, add_comm a 0 ▸ add_zero a,
--- }
+/-
+The below is not actually needed for this project
+/-- Makes an add_comm_monoid in Lean's sense from a more minimal
+set of axioms, deducing zero_add from add_comm and add_zero
+-/
+def add_comm_monoid.mk' {M : Type*} [has_add M] [has_zero M] 
+(add_assoc : ∀ a b c : M, a + b + c = a + (b + c))
+(add_zero : ∀ a : M, a + 0 = a)
+(add_comm : ∀ a b : M, a + b = b + a) :
+add_comm_monoid M :=
+{ add := (+),
+  add_assoc := add_assoc,
+  zero := (0),
+  add_zero := add_zero,
+  add_comm := add_comm,
+  zero_add := λ a, add_comm a 0 ▸ add_zero a,
+}
 
 -- #check add_comm_monoid.mk
+-/
