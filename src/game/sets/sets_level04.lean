12412f4ce4f05787fead1a224c0b8738433916cc
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-06-23 12:43:11 +0100

    adding simp_rw tactic explanation

--- src/game/sets/sets_level04.lean
+++ src/game/sets/sets_level04.lean
@@ -12,7 +12,7 @@ variable X : Type --hide
 ## Level 4
 -/
 
-/- Axiom : eq_iff :
+/- Axiom : ext_iff :
 A = B ↔ ∀ x : X, x ∈ A ↔ x ∈ B
 -/
 
@@ -22,23 +22,14 @@ To prove that two sets are equal, one needs to use the axiom
 of extensionality: two sets are equal if and only if they have
 the same elements.
 
-We have called this axiom `eq_iff`. In mathlib it is called `ext_iff`.
+In Lean's maths library this axiom is called `ext_iff`.
 
 ```
-lemma eq_iff : A = B ↔ ∀ x : X, x ∈ A ↔ x ∈ B
+lemma ext_diff : A = B ↔ ∀ x : X, x ∈ A ↔ x ∈ B
 ```
 -/
 
-/- Hint : Hint
-To prove the theorem below, remember that you can use `split` to 
-change the goal into two goals, corresponding to the left-right and
-right-left implication, respectively. For the first goal, after
-`intro H,` the equality of the two sets can be manipulated
-using `rw eq_iff`.
--/
-
-/-
-## A word on coding style
+/- Hint: A word on coding style
 
 After a `split` statement, one goal turns into two. A good programming style
 would be to use `{}` brackets to work on each goal individually, like this:
@@ -57,43 +48,69 @@ end
 ```
 
 This way you only ever have one goal to work on, and your code becomes
-easier to read.
+easier to read. After `split` you might want to write 
+```
+{ sorry},
+{ sorry}
+```
+so that your code has no errors while you're working on it.
+-/
+
+
+/- Hint : Stuck?
+To prove the theorem below, remember that you can use `split` to 
+change the goal into two goals, corresponding to the left-right and
+right-left implication, respectively. For the first goal, after
+`intro h,` the equality of the two sets can be manipulated
+using `rw ext_iff`.
+-/
+
+/- Hint: rewrite failures
+`rw` doesn't work "under a binder". In other words, if your goal is
+`⊢	∀ (x : X), x ∈ B ↔ x ∈ A ∪ B` then `rw mem_union_iff` won't work!
+It's the `∀` which is blocking it. Either do `intro x` (and then
+the `rw` will work), or use a more powerful rewrite tactic
+called `simp_rw`, which will work 
+
 -/
 
 /- Lemma
 If $A$ and $B$ are sets of any type $X$, then
 $$ A \subseteq B \iff A \cup B = B.$$
 -/
-theorem subset_iff_union_eq (A : set X) (B : set X) : A ⊆ B ↔ A ∪ B = B := 
+theorem subset_iff_union_eq (A : set X) (B : set X) : A ⊆ B ↔ B = A ∪ B := 
 begin
   rw subset_iff,
   split,
   { intro h,
-    rw eq_iff,
-    intro x,
-    rw mem_union_iff,
+    rw ext_iff,
      -- can't rewrite under a binder
+    simp_rw mem_union_iff,
+    intro x,
     specialize h x, -- or replace h := h x,
     tauto! },
   { intro h,
     intros x hA,
-    rw ←h,
+    rw h,
     rw mem_union_iff,
     tauto!
   }
+
+
+
 end
 
--- begin hide
--- theorem subset_iff_union_eq' (A : set X) (B : set X) : A ⊆ B ↔ A ∪ B = B := 
+--begin hide
+-- theorem subset_iff_union_eq' (A : set X) (B : set X) : A ⊆ B ↔ B = A ∪ B := 
 -- begin
 --   rw subset_iff,
---   rw eq_iff,
+--   rw ext_iff,
 --   apply forall_congr,
 --   intro x,
 --   rw mem_union_iff,
 --   tauto!,
 -- end
--- end hide
+--end hide
 
 
 end xena --hide
