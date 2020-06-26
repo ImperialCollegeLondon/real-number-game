3faf3bdc41a6c9ba05b99fe9cbc87a3d7f52bfea
Dan Stanescu <stanescu@uwyo.edu>
2020-06-26 14:03:28 -0600

    Started not_complete_rat proof

--- src/game/sup_inf/rat_complete.lean
+++ src/game/sup_inf/rat_complete.lean
@@ -9,6 +9,8 @@ namespace xena -- hide
 ## Level 13
 
 This proof will be easy now.
+Actually this needs quite some work due to coercions etc.
+May need to change definitions.
 -/
 
 def bdd (X : set ℝ) := bdd_above X ∧ bdd_below X
@@ -28,7 +30,26 @@ begin
    -- S ^ 2 < 2 nor S ^ 2 > 2 (due to density of rationals)
    -- so S ^ 2 = 2, but there's not such S ∈ ℚ
    -- (as per sets/sqrt2NotRational.lean)
-   sorry,
+   intro H,
+   set Y : set ℝ := { y | ∃ x : ℚ, y = ↑x ∧  x ^ 2 < (2:ℚ) } with hY,
+   -- Y is not empty
+   have h1Y : Y.nonempty, sorry,
+   have h2Y : Y ⊆ embedded_rationals, sorry,
+   have h3Y : bdd Y, sorry,
+   have G := H Y (and.intro h1Y (and.intro h2Y h3Y)), 
+   cases G with S hsi, cases hsi with I hsi, cases hsi with hS hI,
+   cases lt_trichotomy (S ^ 2) 2 with hSn hS2,
+   {   -- case S ^ 2 < 2 can be sorted out: rationals are dense
+       sorry, 
+    },
+   cases hS2 with hS hSp, swap,
+   { -- case 2 < S^2 can also be sorted out using density of rationals
+       sorry,
+   },
+   { -- the interesting case S^2 = 2, trouble because of coercions, S is real!!
+        sorry,
+   },
+
 end
 
 end xena -- hide
