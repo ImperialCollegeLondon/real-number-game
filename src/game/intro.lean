6263279905aeae1c6d1a4eb4ff89875b58a8e2fe
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-07-10 21:38:37 +0100

    max minigame!

--- src/game/intro.lean
+++ src/game/intro.lean
@@ -1,39 +1,27 @@
 /-
-# The Real Number Game, version 1.0beta
+# The Max minigame
 
 ## By Kevin Buzzard, Dan Stanescu and Gavin Thomson
 
 # What is this game?
 
-Welcome to the real number game -- a game to help undergraduates learn analysis through Lean,
-a formal proof verification system. Starting from the real numbers with its usual structure,
-we develop the theory of bounds, least upper bounds and greatest lower bounds (sups and infs),
-infinite sequences and infinite series. We develop the theory through problem-solving,
-getting students to formalise proofs in the theory.
+It's just a test of the max levels of the forthcoming real number game. 
 
 This game is a sequel to
 <a href="http://wwwf.imperial.ac.uk/~buzzard/xena/natural_number_game/" target="blank">the natural number game</a>.
-The levels in the Real Number Game need to be solved using tactics. To learn how to use these tactics, I would
+The levels in the Max minigame need to be solved using tactics. To learn how to use these tactics, I would
 recommend that you first play the Natural Number Game up to at least "Advanced Proposition world". I will
 not go through a careful explanation of the tactics taught by the natural number game here.
-
-Blue nodes on the graph are ones that you are ready to enter. Grey nodes you should stay away
-from -- try blue ones higher up the chain first. Green nodes are completed.
 
 # Thanks
 
 Many thanks to Mohammad Pedramfar, without whom this game would not exist.
 
-Several people contributed ideas and sometimes full proofs, most of which have found 
-their place in the game in one way or another.
-This aims at being a complete list eventually: Kenny Lau, Patrick Massot, Christopher Sumnicht, Aniruddh Agarwal.
+# Feedback? 
 
-# Questions?
+Spam us on the discord. This is a work in progress.
 
-You can ask questions on the <a href="https://leanprover.zulipchat.com/" target="blank">Lean Zulip chat</a>,
-where I am often to be found. 
-
-The Real Number Game is brought to you by the Xena project, a project based at Imperial College London
+The Max Minigame is brought to you by the Xena project, a project
 whose aim is to get mathematics undergraduates using computer theorem provers.
 Lean is a computer theorem prover being developed at Microsoft Research.
 
